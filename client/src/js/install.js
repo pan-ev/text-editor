facc8a787bebbc0caa163aadcc2bc4e195f319d0
epan <epan616@gmail.com>
2023-06-18 22:49:46 -0400

    Update install logic and icon

--- client/src/js/install.js
+++ client/src/js/install.js
@@ -1,13 +1,11 @@
 const butInstall = document.getElementById('buttonInstall');
 
 // Logic for installing the PWA
-// TODO: Add an event handler to the `beforeinstallprompt` event
 window.addEventListener('beforeinstallprompt', (event) => {
-    event.preventDefault();
-    butInstall.style.visibility = 'visible';
+    window.deferredPrompt = event;
+    butInstall.classList.toggle('hidden', false);
 });
 
-// TODO: Implement a click event handler on the `butInstall` element
 butInstall.addEventListener('click', async (event) => {
     const promptEvent = window.deferredPrompt;
 
@@ -19,11 +17,9 @@ butInstall.addEventListener('click', async (event) => {
 
     window.deferredPrompt = null;
 
-    butInstall.setAttribute('disabled', true);
-    butInstall.textContent = 'Installed!';
+    butInstall.classList.toggle('hidden', true);
 });
 
-// TODO: Add an handler for the `appinstalled` event
 window.addEventListener('appinstalled', (event) => {
     window.deferredPrompt = null;
 });
